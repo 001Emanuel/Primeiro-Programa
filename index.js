70b7e89ef706b0413f0c278d89fe8ba43ab3c104
001Emanuel <emanuelrenatoreiss@gmail.com>
2023-04-18 21:57:19 -0300

    trocando nome

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port  =  3000; // porta do servidor
 const server = http.createServer((req, res) => { // Criando o servidor
     res.statusCode = 200; // Status ok de resposta
     res.setHeader("Content-Type", "text/plain");
-    res.end('Ola Mundo');
+    res.end('Ola me chamo Emanuel');
 });
 
 server.listen(port, hostname, () => {
