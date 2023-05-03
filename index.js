64ebd8385b07de49448ad5aec9d31492f01a3392
001Emanuel <emanuelrenatoreiss@gmail.com>
2023-05-02 21:13:49 -0300

    Criando páginas com handlesbars

--- index.js
+++ index.js
@@ -2,11 +2,26 @@ const express=require('express');// determinando a utilização do express
 const app=express();
 const path=require ('path');//endereço de cada rota
 const router=express.Router();//trabalha com as rotas
+
+
+const {engine} = require("express-handlebars");
+
+app.engine('handlebars', engine({
+    defaultLayout: 'main',
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+        allowProtoMethodsByDefault: true,
+    },
+
+}))
+
+app.set('view engine', 'handlebars');
 router.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/index.html'));
+    res.render('index');
 })
+
 router.get('/about',function(req,res){
-    res.sendFile(path.join(__dirname+'/about.html'));
+    res.sendFile(oath.join(__dirname+'/about.html'));
 })
 
 app.use('/',router);
